8161e86c3f8ac2f032a2797b61ba71124fb0d354
Marc Sladek <marc@sladek.dev>
2023-06-14 16:08:40 +0200

    CelementsSpringInterceptor use CelementsRequestFilter (#2)
    * use XWikiSessionInitializer
    
    * rename XWikiRequestInitializer
    
    * CelementsRequestFilter
    
    * rename CelementsSpringInterceptor
    
    * rename CelementsSpringInterceptor

---
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelelemntsSpringInterceptor.java
@@ -0,0 +1,34 @@
+package com.celements.spring.mvc;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+import com.celements.init.CelementsRequestFilter;
+
+@Component
+public class CelelemntsSpringInterceptor implements CelMvcInterceptor {
+
+  private final CelementsRequestFilter requestFilter;
+
+  @Inject
+  public CelelemntsSpringInterceptor(CelementsRequestFilter requestFilter) {
+    this.requestFilter = requestFilter;
+  }
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    requestFilter.preExecute("spring", request, response);
+    return true;
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) throws Exception {
+    requestFilter.postExecute();
+  }
+
+}

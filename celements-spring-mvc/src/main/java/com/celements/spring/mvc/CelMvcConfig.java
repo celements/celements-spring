debd177423ec6f70c0d36b65643a4961f9a9438f
Fabian Pichler <fabian.pichler@synventis.com>
2025-04-22 03:49:39 +0200

    CELDEV-1272 - change to springdoc-openapi

--- celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
@@ -6,15 +6,10 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.google.common.collect.ImmutableList;
 
@@ -33,30 +28,7 @@ public class CelMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+    LOGGER.trace("addInterceptors: size {}", interceptors.size());
     interceptors.forEach(registry::addInterceptor);
-  }
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    LOGGER.warn("addResourceHandlers in CelMvcConfig start");
-    // Required for serving the main UI HTML page
-    registry.addResourceHandler("/swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
-
-    // Required for serving CSS/JS/fonts/images used by the UI
-    registry.addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
-
-    LOGGER.warn("addResourceHandlers in CelMvcConfig end");
-  }
-
-  @Bean
-  public ApplicationListener<ContextRefreshedEvent> mappingPrinter(
-      List<RequestMappingHandlerMapping> mappingList) {
-    return event -> {
-      LOGGER.warn("Registered Mappings:");
-      mappingList.stream().forEach(mapping -> mapping.getHandlerMethods()
-          .forEach((key, value) -> LOGGER.warn(" → {}", key)));
-    };
   }
 }
--- celements-spring-mvc/src/main/java/com/celements/spring/mvc/swagger/RequestHandlerPackage.java
+++
@@ -1,9 +0,0 @@
-package com.celements.spring.mvc.swagger;
-
-import java.util.List;
-
-public interface RequestHandlerPackage {
-
-  List<String> basePackages();
-
-}

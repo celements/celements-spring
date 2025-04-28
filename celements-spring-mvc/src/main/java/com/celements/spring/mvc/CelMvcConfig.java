0c698f23b11335028845ed54ab30cb97b3590f96
Fabian Pichler <fabian.pichler@synventis.com>
2025-04-28 23:35:44 +0200

    CELDEV-1272 - add open API / swagger-core suppport (#4)
    * add swagger configuration
    
    * extand RequestHandlerPackage to return String List
    
    * correct SwaggerConfig to handle list of packageNames
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * CELDEV-1272 - change to springdoc-openapi
    
    * debugging
    
    * debugging
    
    * add @Import for Springdoc configuration
    
    * add @Import for Springdoc configuration
    
    * debugging
    
    * finish/cleanup
    
    * finish/cleanup

--- celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +18,14 @@ import com.google.common.collect.ImmutableList;
 
 @Configuration
 @EnableWebMvc
+@EnableConfigurationProperties(SpringDocConfigProperties.class)
+@Import({
+    org.springdoc.core.SpringDocConfiguration.class,
+    org.springdoc.webmvc.core.SpringDocWebMvcConfiguration.class
+})
 public class CelMvcConfig implements WebMvcConfigurer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CelMvcConfig.class);
 
   private final List<CelMvcInterceptor> interceptors;
 
@@ -24,6 +36,7 @@ public class CelMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+    LOGGER.trace("addInterceptors: size {}", interceptors.size());
     interceptors.forEach(registry::addInterceptor);
   }
 }

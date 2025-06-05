fa856951d21342eec0c7dc9508a503394e3a93b2
Fabian Pichler <fabian.pichler@synventis.com>
2025-06-05 02:24:02 +0200

    add debugging

--- celements-spring-mvc/src/main/java/com/celements/spring/security/KeycloakService.java
+++ celements-spring-mvc/src/main/java/com/celements/spring/security/KeycloakService.java
@@ -3,21 +3,29 @@ package com.celements.spring.security;
 import java.util.Optional;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.xwiki.configuration.ConfigurationSource;
 
+import com.celements.configuration.CelementsAllPropertiesConfigurationSource;
 import com.google.common.base.Strings;
 import com.xpn.xwiki.XWikiConstant;
 
 @Component
 public class KeycloakService implements IdentityServer {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(KeycloakService.class);
+
   private final ConfigurationSource configSource;
 
   @Inject
-  public KeycloakService(ConfigurationSource configSource) {
+  public KeycloakService(
+      @Named(CelementsAllPropertiesConfigurationSource.NAME) ConfigurationSource configSource) {
     this.configSource = configSource;
+    LOGGER.info("KeycloakService constructor: {}", configSource.getClass());
   }
 
   @Override

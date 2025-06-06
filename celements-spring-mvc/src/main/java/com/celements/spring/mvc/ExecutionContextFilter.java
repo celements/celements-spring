78eab66aee1c39b6d818fd8bd3c20cc1bf0cd82c
Fabian Pichler <fabian.pichler@synventis.com>
2025-06-06 03:06:21 +0200

    move ExecutionContextFilter to spring-mvc module

---
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/ExecutionContextFilter.java
@@ -0,0 +1,81 @@
+package com.celements.spring.mvc;
+
+import static com.celements.logging.LogUtils.*;
+import static com.celements.spring.context.SpringContextProvider.*;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xwiki.container.servlet.ServletContainerException;
+import org.xwiki.context.Execution;
+import org.xwiki.context.ExecutionContextException;
+
+import com.celements.init.CelementsRequestFilter;
+import com.celements.wiki.WikiMissingException;
+
+public class ExecutionContextFilter implements Filter {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExecutionContextFilter.class);
+
+  private final CelementsRequestFilter requestFilter;
+
+  @Inject
+  public ExecutionContextFilter(CelementsRequestFilter requestFilter) {
+    this.requestFilter = requestFilter;
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    // No initialization needed
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    Execution execution = getBeanFactory().getBean(Execution.class);
+    try {
+      LOGGER.debug("setup execution context for request {}",
+          defer(() -> getRequestUrl(request).orElse("'no HttpServletRequest'")));
+      if ((request instanceof HttpServletRequest) && (response instanceof HttpServletResponse)) {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        requestFilter.preExecute("spring", httpRequest, httpResponse);
+      }
+      chain.doFilter(request, response);
+    } catch (ExecutionContextException | WikiMissingException | ExecutionException
+        | ServletContainerException exp) {
+      LOGGER.error("Failed to execute request becuase initialize execution context failed", exp);
+    } finally {
+      execution.removeContext();
+    }
+  }
+
+  private Optional<String> getRequestUrl(ServletRequest request) {
+    if (request instanceof HttpServletRequest) {
+      HttpServletRequest httpRequest = (HttpServletRequest) request;
+      StringBuffer requestURL = httpRequest.getRequestURL();
+      String queryString = httpRequest.getQueryString();
+      return Optional.of(requestURL.toString() + (queryString != null ? "?" + queryString : ""));
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public void destroy() {
+    // No cleanup needed
+  }
+
+}

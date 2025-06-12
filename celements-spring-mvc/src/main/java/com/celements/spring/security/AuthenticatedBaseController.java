a5da4d06dab1d57a7af1f4fa7abb179c2ded707f
Fabian Pichler <fabian.pichler@synventis.com>
2025-06-12 03:44:00 +0200

    add comment for PreAuthorize usage

--- celements-spring-mvc/src/main/java/com/celements/spring/security/AuthenticatedBaseController.java
+++ celements-spring-mvc/src/main/java/com/celements/spring/security/AuthenticatedBaseController.java
@@ -5,8 +5,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 /**
  * A base controller that enforces that all extending controller methods
  * require an authenticated user by default.
- * This can be overridden on a per-method basis with annotations like @PermitAll
- * or a more specific @PreAuthorize rule.
+ * This can be overridden on a per-method basis with annotations like @PreAuthorize("permitAll()")
+ * or a more specific @PreAuthorize("hasRole('xyz')") rule.
+ * WARNING: do not mix SpEL with JSR-250 annotations as it has issues
  */
 @PreAuthorize("isAuthenticated()")
 public abstract class AuthenticatedBaseController {}

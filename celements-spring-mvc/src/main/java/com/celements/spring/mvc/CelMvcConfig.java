94f4d47188414d6e5e3b1fc52fa08edd39f732ce
Fabian Pichler <fabian.pichler@synventis.com>
2025-06-15 23:40:06 +0200

    CELDEV-1180 - move ExecutionContextFilter to com.celements.init and drop CelementsRequestFilter interceptor

--- celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
@@ -1,20 +1,11 @@
 package com.celements.spring.mvc;
 
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springdoc.core.SpringDocConfigProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.google.common.collect.ImmutableList;
 
 @Configuration
 @EnableWebMvc
@@ -25,18 +16,4 @@ import com.google.common.collect.ImmutableList;
 })
 public class CelMvcConfig implements WebMvcConfigurer {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(CelMvcConfig.class);
-
-  private final List<CelMvcInterceptor> interceptors;
-
-  @Inject
-  public CelMvcConfig(List<CelMvcInterceptor> interceptors) {
-    this.interceptors = ImmutableList.copyOf(interceptors);
-  }
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    LOGGER.trace("addInterceptors: size {}", interceptors.size());
-    interceptors.forEach(registry::addInterceptor);
-  }
 }
--- celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcInterceptor.java
+++
@@ -1,5 +0,0 @@
-package com.celements.spring.mvc;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-
-public interface CelMvcInterceptor extends HandlerInterceptor {}

a8f23ac90c34a93fa6ce4d7b48314cfbd3347dc9
Fabian Pichler <fabian.pichler@synventis.com>
2025-06-25 09:18:02 +0200

    recover CelMvcInterceptor

--- celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcConfig.java
@@ -1,10 +1,17 @@
 package com.celements.spring.mvc;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springdoc.core.SpringDocConfigProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,4 +23,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 })
 public class CelMvcConfig implements WebMvcConfigurer {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CelMvcConfig.class);
+
+  private final List<CelMvcInterceptor> interceptors;
+
+  @Inject
+  public CelMvcConfig(List<CelMvcInterceptor> interceptors) {
+    this.interceptors = List.copyOf(interceptors);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    LOGGER.trace("addInterceptors: size {}", interceptors.size());
+    interceptors.forEach(registry::addInterceptor);
+  }
 }
---
+++ celements-spring-mvc/src/main/java/com/celements/spring/mvc/CelMvcInterceptor.java
@@ -0,0 +1,5 @@
+package com.celements.spring.mvc;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public interface CelMvcInterceptor extends HandlerInterceptor {}

1720c937d2526211f0989ebd086fa9679fcb61a2
Fabian Pichler <fabian.pichler@synventis.com>
2025-08-05 23:12:10 +0200

    introduce OAuth2BeansConfig to solve circular dep

---
+++ celements-spring-security/src/main/java/com/celements/spring/security/OAuth2BeansConfig.java
@@ -0,0 +1,18 @@
+package com.celements.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+@Configuration
+public class OAuth2BeansConfig {
+
+  @Bean
+  public OAuth2AuthorizedClientService authorizedClientService(
+      ClientRegistrationRepository clientRegistrationRepository) {
+    return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+  }
+
+}

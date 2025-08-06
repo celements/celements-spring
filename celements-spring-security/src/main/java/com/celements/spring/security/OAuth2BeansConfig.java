8077bf320e2856007c0d0e7a6d97bb78ca973e65
Fabian Pichler <fabian.pichler@synventis.com>
2025-08-06 20:45:15 +0200

    add authorizedClientRepository and authorizedClientManager

--- celements-spring-security/src/main/java/com/celements/spring/security/OAuth2BeansConfig.java
+++ celements-spring-security/src/main/java/com/celements/spring/security/OAuth2BeansConfig.java
@@ -7,8 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.xwiki.context.Execution;
 
 import com.celements.execution.XWikiExecutionProp;
@@ -38,6 +44,30 @@ public class OAuth2BeansConfig {
   public OAuth2AuthorizedClientService authorizedClientService(
       ClientRegistrationRepository clientRegistrationRepository) {
     return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientRepository authorizedClientRepository(
+      OAuth2AuthorizedClientService clientService) {
+    return new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(clientService);
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientManager authorizedClientManager(
+      ClientRegistrationRepository clientRegistrations,
+      OAuth2AuthorizedClientRepository authorizedClients) {
+
+    // 1) build the provider that knows how to handle code & refresh flows
+    OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder()
+        .authorizationCode()
+        .refreshToken()
+        .build();
+
+    // 2) wire it into the default manager
+    DefaultOAuth2AuthorizedClientManager manager = new DefaultOAuth2AuthorizedClientManager(
+        clientRegistrations, authorizedClients);
+    manager.setAuthorizedClientProvider(provider);
+    return manager;
   }
 
 }

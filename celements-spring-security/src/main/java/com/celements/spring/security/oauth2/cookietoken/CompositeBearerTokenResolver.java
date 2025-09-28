75e6c3962af33eabdc25da0c4d87fbd24f8ef56e
Fabian Pichler <fabian.pichler@synventis.com>
2025-09-28 23:22:24 +0200

    clean/refactor CompositeBearerTokenResolver

--- celements-spring-security/src/main/java/com/celements/spring/security/oauth2/cookietoken/CompositeBearerTokenResolver.java
+++ celements-spring-security/src/main/java/com/celements/spring/security/oauth2/cookietoken/CompositeBearerTokenResolver.java
@@ -1,32 +1,36 @@
 package com.celements.spring.security.oauth2.cookietoken;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.server.resource.BearerTokenError;
 import org.springframework.security.oauth2.server.resource.BearerTokenErrors;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 
 public class CompositeBearerTokenResolver implements BearerTokenResolver {
 
-  private final BearerTokenResolver header = new DefaultBearerTokenResolver();
-  private final BearerTokenResolver cookie;
+  private final BearerTokenResolver headerResolver = new DefaultBearerTokenResolver();
+  private final BearerTokenResolver cookieResolver;
 
   public CompositeBearerTokenResolver(BearerTokenResolver cookie) {
-    this.cookie = cookie;
+    this.cookieResolver = cookie;
   }
 
   @Override
   public String resolve(HttpServletRequest request) {
-    String fromCookie = cookie.resolve(request);
-    String fromHeader = header.resolve(request);
-
-    if ((fromCookie != null) && (fromHeader != null) && !fromCookie.equals(fromHeader)) {
-      BearerTokenError error = BearerTokenErrors
-          .invalidRequest("Found multiple bearer tokens in the request");
-      throw new OAuth2AuthenticationException(error);
+    LinkedHashSet<String> tokens = Stream.of(cookieResolver, headerResolver)
+        .map(resolver -> resolver.resolve(request))
+        .filter(Objects::nonNull)
+        .collect(Collectors.toCollection(LinkedHashSet::new));
+    if (tokens.size() > 1) {
+      throw new OAuth2AuthenticationException(
+          BearerTokenErrors.invalidRequest("Found multiple bearer tokens in the request"));
     }
-    return (fromCookie != null) ? fromCookie : fromHeader;
+    return tokens.stream().findFirst().orElse(null);
   }
 }

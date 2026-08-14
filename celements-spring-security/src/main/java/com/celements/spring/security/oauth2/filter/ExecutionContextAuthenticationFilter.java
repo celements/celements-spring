c0c47fbb60a35dade471d43aa5c1292447d03dfd
Marc Sladek <marc@sladek.dev>
2026-08-15 01:02:53 +0200

    CELDEV-1348 Auto Login

--- celements-spring-security/src/main/java/com/celements/spring/security/oauth2/filter/ExecutionContextAuthenticationFilter.java
+++ celements-spring-security/src/main/java/com/celements/spring/security/oauth2/filter/ExecutionContextAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.celements.spring.security.oauth2.filter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -11,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -20,32 +23,47 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.xwiki.context.Execution;
 
+import com.celements.auth.MainAdminConfig;
 import com.celements.auth.user.User;
+import com.celements.auth.user.UserInstantiationException;
 import com.celements.auth.user.UserService;
 import com.celements.execution.XWikiExecutionProp;
+import com.xpn.xwiki.user.api.XWikiUser;
 
 public class ExecutionContextAuthenticationFilter extends OncePerRequestFilter {
 
   private static final Logger LOGGER = LoggerFactory
       .getLogger(ExecutionContextAuthenticationFilter.class);
 
-  private final Execution execution;
   private final UserService userService;
+  private final MainAdminConfig mainAdminConfig;
+  private final Execution execution;
 
-  public ExecutionContextAuthenticationFilter(UserService userService, Execution execution) {
+  public ExecutionContextAuthenticationFilter(
+      UserService userService,
+      MainAdminConfig mainAdminConfig,
+      Execution execution) {
     this.userService = userService;
+    this.mainAdminConfig = mainAdminConfig;
     this.execution = execution;
   }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    var securityContext = SecurityContextHolder.getContext();
+    autoLogin(securityContext)
+        .or(() -> processAuthentication(securityContext))
+        .ifPresent(user -> execution.getContext().set(XWikiExecutionProp.XWIKI_USER, user));
+    filterChain.doFilter(request, response);
+  }
+
+  private Optional<XWikiUser> processAuthentication(SecurityContext securityContext) {
+    Authentication auth = securityContext.getAuthentication();
     if ((auth != null) && auth.isAuthenticated()) {
       String userId = auth.getName(); // the Keycloak GUID (sub)
       String username;
       String email;
-
       if (auth instanceof JwtAuthenticationToken) {
         JwtAuthenticationToken jwtAuth = (JwtAuthenticationToken) auth;
         Jwt jwt = jwtAuth.getToken();
@@ -70,7 +88,7 @@ public class ExecutionContextAuthenticationFilter extends OncePerRequestFilter {
           User user = userOpt.get();
           LOGGER.debug("setting xwiki isGlobal='{}', username='{}', email='{}'", user.isGlobal(),
               user.asXWikiUser().getUser(), user.email());
-          execution.getContext().set(XWikiExecutionProp.XWIKI_USER, user.asXWikiUser());
+          return Optional.of(user.asXWikiUser());
         } else {
           LOGGER.info("no celements user found for email='{}'", email);
         }
@@ -81,7 +99,17 @@ public class ExecutionContextAuthenticationFilter extends OncePerRequestFilter {
     } else {
       LOGGER.info("is NOT authenticated with user '{}'", "null");
     }
-    filterChain.doFilter(request, response);
+    return Optional.empty();
+  }
+
+  private Optional<XWikiUser> autoLogin(SecurityContext securityContext) {
+    if (!mainAdminConfig.isAutoLoginEnabled()) {
+      return Optional.empty();
+    }
+    var xwikiUser = mainAdminConfig.getXWikiUser();
+    var auth = new UsernamePasswordAuthenticationToken(xwikiUser.getUser(), null, List.of());
+    securityContext.setAuthentication(auth);
+    return Optional.of(xwikiUser);
   }
 
 }
